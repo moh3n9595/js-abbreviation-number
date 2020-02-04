0e0a542e64522e3fe7af2a6d35b405c657fafdfb
moh3n9595 <mohsenando@gmail.com>
2020-02-04 12:21:35 +0330

    add prettier

--- index.ts
+++ index.ts
@@ -1,28 +1,27 @@
+export function abbreviateNumber(
+  num: number,
+  digit: number = 1,
+  symbols: Array<string> = ["", "K", "M", "G", "T", "P", "E"],
+): string {
+  // handle negatives
+  const sign = Math.sign(num) >= 0;
+  num = Math.abs(num);
 
+  // what tier? (determines SI symbol)
+  const tier = (Math.log10(num) / 3) | 0;
 
-export function abbreviateNumber(num: number, digit: number = 1, symbols: Array<string> = ["", "K", "M", "G", "T", "P", "E"]): string {
+  // if zero, we don't need a suffix
+  if (tier == 0) return num.toString();
 
-    // handle negatives
-    const sign = Math.sign(num) >= 0;
-    num = Math.abs(num);
+  // get suffix and determine scale
+  const suffix = symbols[tier];
+  if (!suffix) throw new RangeError();
 
-    // what tier? (determines SI symbol)
-    const tier = Math.log10(num) / 3 | 0;
+  const scale = Math.pow(10, tier * 3);
 
-    // if zero, we don't need a suffix
-    if(tier == 0) return num.toString();
+  // scale the number
+  const scaled = num / scale;
 
-    // get suffix and determine scale
-    const suffix = symbols[tier];
-    if(!suffix)
-        throw new RangeError();
-
-    const scale = Math.pow(10, tier * 3);
-
-    // scale the number
-    const scaled = num / scale;
-
-    // format number and add suffix
-    return (!sign?"-":"") + scaled.toFixed(digit) + suffix;
-
+  // format number and add suffix
+  return (!sign ? "-" : "") + scaled.toFixed(digit) + suffix;
 }
